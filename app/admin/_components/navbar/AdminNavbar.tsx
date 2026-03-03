211a4cf02bb631d06375477a3c51fb52fa714d68
Tobi-Oyedele <tobiloba.oyedele05@gmail.com>
2026-03-03 21:20:30 +0100

    feat: add power icon for logout on admin dashboard

--- app/admin/_components/navbar/AdminNavbar.tsx
+++ app/admin/_components/navbar/AdminNavbar.tsx
@@ -3,6 +3,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Power } from "lucide-react";
+import { logout } from "@/lib/api/auth";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/store/authStore";
 
 const navLinks = [
   { label: "Dashboard", href: "/admin/dashboard" },
@@ -12,10 +17,23 @@ const navLinks = [
 ];
 
 export default function AdminNavbar() {
+  const { clearAuth } = useAuthStore();
   const pathname = usePathname();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      clearAuth();
+      toast.success("Logged out successfully!");
+      router.push("/admin/sign-in");
+    } catch {
+      toast.error("Something went wrong. Please try again.");
+    }
+  };
 
   return (
-    <nav className="w-full bg-ivory px-8 py-4 flex items-center gap-12">
+    <nav className="w-full bg-ivory px-8 py-4 flex items-center justify-between gap-12">
       <Image
         src="/images/Logo.svg"
         alt="Saint Valor Logo"
@@ -42,6 +60,12 @@ export default function AdminNavbar() {
           );
         })}
       </ul>
+
+      <div>
+        <button type="button" className="cursor-pointer" onClick={handleLogout}>
+          <Power />
+        </button>
+      </div>
     </nav>
   );
 }
