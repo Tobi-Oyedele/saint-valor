a8a5416b2d5550913932e5b1cbb1c152a6656967
Tobi-Oyedele <tobiloba.oyedele05@gmail.com>
2026-03-25 10:39:08 +0100

    fix: remove stale onAddToCart prop from ShopGrid ProductCard usage

--- components/shop/ShopGrid.tsx
+++ components/shop/ShopGrid.tsx
@@ -34,11 +34,7 @@ const ShopGrid = ({ products, isLoading }: ShopGridProps) => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-4 gap-x-4 gap-y-8">
       {products.map((product) => (
-        <ProductCard
-          key={product._id}
-          product={product}
-          onAddToCart={() => {}}
-        />
+        <ProductCard key={product._id} product={product} />
       ))}
     </div>
   );
